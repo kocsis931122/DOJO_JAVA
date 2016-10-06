c8ddc6611fab2d4eae618680cad9f2598e979dff
Kocsis Viktor <achilleus88888@gmail.com>
2016-10-06 10:12:21 +0200

    Finish task

---
+++ src/printTree/PrintTree.java
@@ -0,0 +1,20 @@
+package printTree;
+
+public class PrintTree {
+
+	public static void printNiceText(String text) {
+		char[] chars = text.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			for (int j = 0; j < chars.length - i; j++)
+				System.out.print(" ");
+			for (int k = 0; k < (2 * i + 1); k++)
+				System.out.print(chars[i]);
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		printNiceText("apacuka");
+	}
+
+}
